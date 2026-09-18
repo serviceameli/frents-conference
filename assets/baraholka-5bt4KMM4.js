38284116cf89982b9f192196f30e08b7ecb70002
serviceameli <service.ameli.aniri@gmail.com>
2026-09-18 16:29:09 +0300

    Publish six Frents services and upcoming-service applications

--- assets/baraholka-5bt4KMM4.js
+++
@@ -1,1 +0,0 @@
-import{n as e,r as t,t as n}from"./compact-YpMUWoIC.js";var r=t(),i=e(),a=document.getElementById(`root`);if(!a)throw Error(`Missing page root`);(0,r.createRoot)(a).render((0,i.jsx)(n,{initialService:`market`}));
